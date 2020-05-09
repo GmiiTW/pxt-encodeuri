7c254ff506af562736b1bb6202d40a709f405ddc
STLeee <a8365885@hotmail.com>
2020-05-10 01:15:53 +0800

    test

--- main.ts
+++ main.ts
@@ -3,12 +3,12 @@ namespace encodeuri {
     //% block="encodeURI $str"
     //% inlineInputMode=inline
     export function encode(str: string): string {
-        return encodeURI(str)
+        return "encodeURI(str)"
     }
 
     //% block="decodeURI $str"
     //% inlineInputMode=inline
     export function decode(str: string): string {
-        return decodeURI(str)
+        return "decodeURI(str)"
     }
 }
