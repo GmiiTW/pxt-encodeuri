0bfb21df6785f1837515dc512248a3e5f852aa40
STLeee <a8365885@hotmail.com>
2020-05-10 01:36:39 +0800

    test

--- main.ts
+++ main.ts
@@ -1,14 +1,14 @@
-//% color="#0000FF" weight=10 icon="\uf121"
+//% color="#00FF00" weight=10 icon="\uf121"
 namespace encodeuri {
     //% block="encodeURI $str"
     //% inlineInputMode=inline
     export function encode(str: string): string {
-        return "encodeURI(str)"
+        return encodeURI(str)
     }
 
     //% block="decodeURI $str"
     //% inlineInputMode=inline
     export function decode(str: string): string {
-        return "decodeURI(str)"
+        return decodeURI(str)
     }
 }
