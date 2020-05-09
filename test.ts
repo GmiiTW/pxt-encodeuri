bae1cf8b85752bdb75ace01cafaf9e334076664d
STLeee <a8365885@hotmail.com>
2020-05-10 01:11:58 +0800

    update

--- test.ts
+++
@@ -1,7 +0,0 @@
-// tests go here; this will not be compiled when this package is used as a library
-
-var str = "test 測試"
-var encoded = encodeuri.encode(str)
-console.log(`encoded: ${encoded}`)
-var decoded = encodeuri.decode(encoded)
-console.log(`decoded: ${decoded}`)
